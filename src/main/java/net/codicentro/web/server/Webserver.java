fb656d27895a0914a381bc30183df6cac31a302a
avillalobos <avillalobos@avillalobos-lap>
2015-01-24 12:25:42 -0600

    Test

--- src/main/java/net/codicentro/web/server/Webserver.java
+++ src/main/java/net/codicentro/web/server/Webserver.java
@@ -14,21 +14,36 @@
  **/
 package net.codicentro.web.server;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import net.codicentro.core.TypeCast;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.xml.XmlConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Webserver {
 
+    // private static final Logger logger = LoggerFactory.getLogger("/home/data/workspace/codicentro-webserver/target/conf/log4j.properties");
     public static void main(String[] args) throws Exception {
-        Resource resource = Resource.newSystemResource("/jetty8-http.xml");
+//        for (String property : System.getProperties().stringPropertyNames()) {
+//            System.out.println(property + "=" + System.getProperty(property));
+//        }
+
+        System.out.println("JAVA_HOME=" + System.getenv("JAVA_HOME"));
+//        String WEBSERVER_HOME = TypeCast.isBlank(System.getenv("WEBSERVER_HOME")) ? System.getProperty("java.class.path") : System.getenv("WEBSERVER_HOME");
+        String WEBSERVER_HOME = System.getenv("WEBSERVER_HOME");
+        System.out.println("WEBSERVER_HOME=" + WEBSERVER_HOME);
+
+        Resource resource = Resource.newResource(WEBSERVER_HOME + "/conf/jetty8-http.xml");
         XmlConfiguration config = new XmlConfiguration(resource.getInputStream());
         Server server = (Server) config.configure();
         HandlerList handlers = new HandlerList();
         for (String arg : args) {
-            resource = Resource.newSystemResource("/jetty-web-" + arg + ".xml");
+            resource = Resource.newResource(WEBSERVER_HOME + "/conf/jetty-web-" + arg + ".xml");
             config = new XmlConfiguration(resource.getInputStream());
             WebAppContext wac = (WebAppContext) config.configure();
             wac.setContextPath("/" + arg);
